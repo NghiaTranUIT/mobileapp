4ce28bfd28eb2dc35274421dcd02e3d14687e427
khoi <khoiracle@gmail.com>
2018-10-30 14:18:46 +0700

    Add CreateEntityViewCell

---
+++ Toggl.Daneel/Views/EntityCreation/CreateEntityViewCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Toggl.Daneel.Views.EntityCreation
+{
+    public partial class CreateEntityViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("CreateEntityViewCell");
+        public static readonly UINib Nib;
+
+        static CreateEntityViewCell()
+        {
+            Nib = UINib.FromName("CreateEntityViewCell", NSBundle.MainBundle);
+        }
+
+        protected CreateEntityViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
+
